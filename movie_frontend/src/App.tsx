63ed811492988bff558c1b74723236b3a1980296
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2025-09-28 14:39:44 +0200

    initial commit

---
+++ movie_frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <p>
+          Welcome to my movie homepage. Features will be added as times goes!
+        </p>
+    </>
+  )
+}
+
+export default App
